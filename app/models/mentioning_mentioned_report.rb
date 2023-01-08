82f632f9de8d72c31e2e987546c4657d247aafd6
choco <superbeaver.180809@gmail.com>
2023-01-08 11:45:19 +0900

    関連付けを作成

--- app/models/mentioning_mentioned_report.rb
+++ app/models/mentioning_mentioned_report.rb
@@ -1,2 +1,4 @@
 class MentioningMentionedReport < ApplicationRecord
+  belongs_to :mentioning_reports, class_name: 'Report'
+  belongs_to :mentioned_reports, class_name: 'Report'
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -4,6 +4,23 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
+  has_many :mentioning,
+           class_name: 'MentioningMentionedReport',
+           foreign_key: 'mentioned_report_id',
+           inverse_of: :mentioned_reports,
+           dependent: :destroy
+  # 引用先の日報を求める場合
+  has_many :mentioning_reports, through: :mentioning, source: :mentioning_reports
+
+  has_many :mentioned,
+           class_name: 'MentioningMentionedReport',
+           foreign_key: 'mentioning_report_id',
+           inverse_of: :mentioning_reports,
+           dependent: :destroy
+
+  has_many :mentioned_reports, through: :mentioned, source: :mentioned_reports
+
+
   validates :title, presence: true
   validates :content, presence: true
 
