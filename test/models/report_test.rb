47c9fefa5219998088732fd2f87d18937693bf6e
choco <superbeaver.180809@gmail.com>
2023-01-15 15:07:10 +0900

    テストケースを修正

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -4,38 +4,45 @@ require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
   setup do
-    @report1 = reports(:one)
-    @report2 = reports(:two)
-    @report3 = reports(:three)
+    @new_report = Report.new(user_id: users(:alice).id,
+                             title: 'railsの学習が難しい',
+                             content: 'おすすめの参考サイトが知りたい')
+    @another_report_url = "http://localhost:3000/reports/#{reports(:report1).id}"
   end
 
-  test 'MentioningMentionedReport should be count up when mentioning' do
-    @report1.content = "http://localhost:3000/reports/#{@report2.id}"
-    @report1.save_mentioning_reports
+  test '別の日報を言及していない時、MentioningMentionedReportの数は変わらない' do
+    assert_no_difference 'MentioningMentionedReport.count' do
+      @new_report.save_or_update_report_and_update_mentioning_reports
+    end
+  end
+
+  test '別の日報を言及した時、MentioningMentionedReportの数が増加する' do
+    @new_report[:content] += @another_report_url
     assert_difference 'MentioningMentionedReport.count', 1 do
-      @report1.save_mentioning_reports
+      @new_report.save_or_update_report_and_update_mentioning_reports
     end
   end
 
-  test 'MentioningMentionedReport should be count dont matter when dont mentioning' do
-    assert_no_difference 'MentioningMentionedReport.count' do
-      @report1.save_mentioning_reports
+  test '別の日報を複数言及した時、MentioningMentionedReportの数が言及した分増加する' do
+    @new_report[:content] += @another_report_url + "http://localhost:3000/reports/#{reports(:report2).id}"
+    assert_difference 'MentioningMentionedReport.count', 2 do
+      @new_report.save_or_update_report_and_update_mentioning_reports
     end
-    assert_empty @report1.mentioning_reports
   end
 
-  test 'MentioningMentionedReport should be count dont matter when another mentioning' do
-    @report3.content = "http://localhost:3000/reports/#{@report1.id}"
-    assert_no_difference 'MentioningMentionedReport.count' do
-      @report3.update_mentioning_reports
+  test '同じ日報を複数回言及した時、MentioningMentionedReportの数は1つだけ増加する' do
+    @new_report[:content] += @another_report_url + @another_report_url
+    assert_difference 'MentioningMentionedReport.count', 1 do
+      @new_report.save_or_update_report_and_update_mentioning_reports
     end
-    assert_equal @report3, @report1.mentioned_reports[0]
   end
 
-  test 'MentioningMentionedReport should be count down when dont mentioning' do
-    @report3.content = '日報へのリンクなし'
+  test '既に日報を言及していた状態から、言及先を変更するとMentioningMentionedReportの数が減少する' do
+    @new_report[:content] += @another_report_url + "http://localhost:3000/reports/#{reports(:report2).id}"
+    @new_report.save_or_update_report_and_update_mentioning_reports
     assert_difference 'MentioningMentionedReport.count', -1 do
-      @report3.update_mentioning_reports
+      @new_report.save_or_update_report_and_update_mentioning_reports(content: @another_report_url)
     end
   end
+
 end
