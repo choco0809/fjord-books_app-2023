5054c8d4574a91ec850d80351cfaedcd3695686b
choco <superbeaver.180809@gmail.com>
2023-01-15 12:48:59 +0900

    名詞に変更した

--- app/models/report.rb
+++ app/models/report.rb
@@ -6,21 +6,21 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
-  has_many :mentioning,
+  has_many :active_mention,
            class_name: 'MentioningMentionedReport',
            foreign_key: 'mentioning_report_id',
            inverse_of: :mentioning_report,
            dependent: :destroy
 
-  has_many :mentioning_reports, through: :mentioning, source: :mentioned_report
+  has_many :mentioning_reports, through: :active_mention, source: :mentioned_report
 
-  has_many :mentioned,
+  has_many :passive_mention,
            class_name: 'MentioningMentionedReport',
            foreign_key: 'mentioned_report_id',
            inverse_of: :mentioned_report,
            dependent: :destroy
 
-  has_many :mentioned_reports, through: :mentioned, source: :mentioning_report
+  has_many :mentioned_reports, through: :passive_mention, source: :mentioning_report
 
   validates :title, presence: true
   validates :content, presence: true
